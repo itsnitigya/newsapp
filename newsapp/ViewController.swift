e2cdc3144b52ab6e2d84ce8268e3365f3eabb0da
Nitigya Kapoor <kapoornitigya@gmail.com>
2022-04-12 17:01:22 +0530

    Initial Commit

---
+++ newsapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsapp
+//
+//  Created by Nitigya Kapoor on 12/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
